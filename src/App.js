3a355e010c2dc85f942018bcdcee190252d158e0
Vaibhav4556 <107250286+Vaibhav4556@users.noreply.github.com>
2022-07-09 14:07:12 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
   <div class="container">
     
       
-      <Card subdomain="Free Subdomain" storage="5GB" user="Single User" price="$0" title="Free" />
-      <Card subdomain="Free Subdomain" storage="50GB" user="5 Users" price="$9" title="Plus" />
+      <Card subdomain="Free Subdomain" storage="5GB" user="Single User" price="$0" title="Free" /><hr />
+      <Card subdomain="Free Subdomain" storage="50GB" user="5 Users" price="$9" title="Plus" /><hr />
       <Card subdomain="Unlimited Free Subdomain" storage="150GB" user="Unlimited Users" price="$49" title="Pro" />
      </div>
     
