f7ca685fd8acb866ef415266033696d7ab7f783a
unknown <your name>
2022-07-09 12:39:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Card from "./Card.js";
+
+export default function App() {
+  return (
+  <section class="pricing py-5">
+  <div class="container">
+    
+      
+      <Card subdomain="Free Subdomain" storage="5GB" user="Single User" price="$0" title="Free" />
+      <Card subdomain="Free Subdomain" storage="50GB" user="5 Users" price="$9" title="Plus" />
+      <Card subdomain="Unlimited Free Subdomain" storage="150GB" user="Unlimited Users" price="$49" title="Pro" />
+     </div>
+    
+  
+     </section>
+  )
+}
---
+++ src/Card.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Card =(props)=>{
+
+  return(
+    <div>
+      <div class="row">
+       <div className="col-lg-4">
+      <div className="card mb-5 mb-lg-0">
+        <div className="card-body">
+          <h5 className="card-title text-muted text-uppercase text-center">{props.title}</h5>
+          <h6 className="card-price text-center">{props.price}<span className="period">/month</span></h6><hr/>
+        
+          <ul className="fa-ul">
+            <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.user}</li>
+            <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.storage}</li>
+            <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+            <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
+              Private Projects</li>
+            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
+              Phone Support</li>
+            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{props.subdomain}
+            </li>
+            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
+              Reports</li>
+          </ul>
+          <div className="d-grid">
+            <a href="#" className="btn btn-primary text-uppercase">Button</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+     </div> )
+  }
+
+
+export default Card;
